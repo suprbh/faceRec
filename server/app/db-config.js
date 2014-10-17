279a74a48fd5a513705fc8e4d68c88940a28de5a
Ryo Osawa <ryojiosawa@gmail.com>
2014-10-16 18:05:56 -0700

    Delete views and db dirs.  Externalize MONGOLAB_URI as an environment variable

--- server/app/db-config.js
+++ server/app/db-config.js
@@ -2,18 +2,14 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var path = require('path');
 
-if (process.env.PORT === undefined){
-  process.env.mongoURI = 'mongodb://localhost:27017/shortlydb';
-} else {
-  process.env.mongoURI = 'mongodb://MongoLab-n:e2CREwn6qmW0zC4yjKtrsS3NUrS6KeXQ8r17NXL8Xm0-@ds041167.mongolab.com:41167/MongoLab-n';
-
-}
+mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/easy-auth';
+mongoose.connect(mongoURI);
 mongoose.connect(process.env.mongoURI);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
-  // yay!
+  console.log('Mongodb connection open');
 });
 
 module.exports = db;
