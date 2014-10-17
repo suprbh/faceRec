02f7a6455cc0f0eadfcefdcec16ce350b104be18
Allen <allenkrulwich@gmail.com>
2014-10-16 17:26:47 -0700

    added explanatory comments to module interface methods

--- modules/passwordModule/module.js
+++ modules/passwordModule/module.js
@@ -1,6 +1,15 @@
+// The module object will be assigned to module.exports
 var passwordModule = {
+
+  // moduleName is how the server routes incoming HTTP requests,
+  // EG, a get to EasyAuth.com/password will be routed to the module 'password'
   moduleName: 'password',
+
+  // Incoming authentication requests will come by this method 
   method: 'POST',
+
+  // When a user wants to set up a module, this function is called and
+  // rendered at easyAuth.com/moduleName/setup
   setupRender: function(){
     $element = $('<form></form>').on('submit', function(e){
       $.ajax({
@@ -13,6 +22,9 @@ var passwordModule = {
     .append('<button type = "submit"> Submit </button>');
     return $element;
   },
+
+  // When a user wants to login with a module, this function is called and
+  // rendered at easyAuth.com/moduleName/login
   loginRender: function(){
     $element = $('<form></form>').on('submit', function(e){
       $.ajax({
@@ -25,10 +37,16 @@ var passwordModule = {
     .append('<button type = "submit"> Submit </button>');
     return $element;
   },
+
+  // When the server recieves an HTTP request to EasyAuth.com/moduleName/setup/submit,
+  // the server calls module.setup() with the request data. 
   setup: function(providedUserData){
     // Process providedUserData, eg by hashing
     return userDataForStorage;
   },
+
+  // When the server recieves an HTTP request to EasyAuth.com/moduleName/setup/submit,
+  // the server calls module.setup() with the request data and the previous stored user data.
   login: function(providedUserData, storedUserData){
     //if (providedUserData === storedUserData){
     //  return true;
@@ -37,16 +55,10 @@ var passwordModule = {
   },
 }
 
-module.exports = passwordModule;
-
-/*
-
-
-  function(storedUserData){
-    var userProvidedPassword = $('#passwordField').val();
-    if ( checkPassword( userProvidedPassword, storedUserData.passwordHash) )
-  };
-  var checkPassword = function(userProvidedPassword, storedPasswordHash){
+var utils = {};
+utils.checkPassword = function(userProvidedPassword, storedPasswordHash){
     //TODO: Actually check the password
     return true;
-  };*/
+};
+
+module.exports = passwordModule;
