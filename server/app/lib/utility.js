91e39bd7e34151ccc357e56aa320cb89de3ae095
Ryo Osawa <ryojiosawa@gmail.com>
2014-10-20 13:18:26 -0700

    Delete duplicate /lib and add a new post route to /login to redirect user to index page

--- server/app/lib/utility.js
+++ server/app/lib/utility.js
@@ -1,19 +1,3 @@
-var request = require('request');
-
-exports.getUrlTitle = function(url, cb) {
-  request(url, function(err, res, html) {
-    if (err) {
-      console.log('Error reading url heading: ', err);
-      return cb(err);
-    } else {
-      var tag = /<title>(.*)<\/title>/;
-      var match = html.match(tag);
-      var title = match ? match[1] : url;
-      return cb(err, title);
-    }
-  });
-};
-
 var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
 
 exports.isValidUrl = function(url) {
@@ -35,6 +19,6 @@ exports.checkUser = function(req, res, next) {
 exports.createSession = function(req, res, newUser) {
   return req.session.regenerate(function() {
       req.session.user = newUser;
-      res.redirect('/');
+      res.redirect('/index');
     });
 };
