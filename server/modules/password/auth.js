136e7d6ac1838c8e2dc7a9e018be07324984f315
Allen <allenkrulwich@gmail.com>
2014-10-22 11:45:43 -0700

    Added basic functionality to the password module

--- server/modules/password/auth.js
+++ server/modules/password/auth.js
@@ -1,10 +1,10 @@
 // The module object will be assigned to module.exports
 var fs = require('fs');
-var passwordModule = {
+var bcrypt = require('bcrypt-nodejs');
 
-  // moduleName is how the server routes incoming HTTP requests,
-  // EG, a get to EasyAuth.com/password will be routed to the module 'password'
-  moduleName: 'password',
+var db = require('../../app/controllers/controller.js');
+
+var passwordModule = {
   
   // When a user wants to set up a module, this function is called and
   // rendered at GET easyAuth.com/password/setup
@@ -32,20 +32,45 @@ var passwordModule = {
   // When the server recieves an HTTP request to POST EasyAuth.com/password/setup/
   // the server calls module.setup() with the request data. 
   setup: function(req, res, next){
-
+    var userProvidedPassword = req.body.password;
+    var userProvidedPasswordHash = utils.hashPassword(userProvidedPassword, function(hash){
+      var task = {};
+      task.password = userProvidedPasswordHash;
+      db.writeAuthTask(username, authName, task, function(error, authTask, user){
+        next();
+      });
+    });
   },
 
   // When the server recieves an HTTP request to POST EasyAuth.com/moduleName/setup/,
   // the server calls module.setup() with the request data and the previous stored user data.
   auth: function(req, res, next){
-    return false;
+    var username = req.body.username;
+    db.readAuthTask(username, 'password', function(error, authTask, user){
+      var storedPasswordHash = user.password;
+      var userProvidedPassword = req.body.password;
+      var userProvidedPasswordHash = utils.hashPassword(userProvidedPassword);
+      utils.comparePassword(userProvidedPassword, storedPasswordHash, function(isMatch){
+        if (isMatch){
+          next();
+        } else {
+          res.status(403).send('Failed Authentication');
+        }
+      });
+    });
   },
 }
 
 var utils = {};
-utils.checkPassword = function(userProvidedPassword, storedPasswordHash){
-    //TODO: Actually check the password
-    return true;
+utils.comparePassword = function(userProvidedPasswordHash, storedPasswordHash, callback){
+  bcrypt.compare(userProvidedPasswordHash, storedPasswordHash, function(err, isMatch) {
+    callback(isMatch);
+  });
 };
+utils.hashPassword = function(userProvidedPassword, callback){
+  bcrypt.hash(userProvidedPassword, null, function(err, hash){
+    callback(hash);
+  });
+}
 
 module.exports = passwordModule;
