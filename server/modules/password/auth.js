89bff4c038bd338dba0923a9aaba11f3ac3d25c0
Allen <allenkrulwich@gmail.com>
2014-10-22 12:08:08 -0700

    changed password module routes and updated forms for setup and auth

--- server/modules/password/auth.js
+++ server/modules/password/auth.js
@@ -1,7 +1,6 @@
 // The module object will be assigned to module.exports
 var fs = require('fs');
 var bcrypt = require('bcrypt-nodejs');
-
 var db = require('../../app/controllers/controller.js');
 
 var passwordModule = {
@@ -10,7 +9,6 @@ var passwordModule = {
   // rendered at GET easyAuth.com/password/setup
   setupRender: function(req, res){
     var options = {};
-    console.log(fs.readdirSync('.'));
     fs.readFile('./server/modules/password/views/setup.html', function(err, data){
       if (err) throw err;
       res.set('Content-Type', 'text/html');
@@ -32,12 +30,13 @@ var passwordModule = {
   // When the server recieves an HTTP request to POST EasyAuth.com/password/setup/
   // the server calls module.setup() with the request data. 
   setup: function(req, res, next){
+    var username = req.body.username;
     var userProvidedPassword = req.body.password;
-    var userProvidedPasswordHash = utils.hashPassword(userProvidedPassword, function(hash){
+    utils.hashPassword(userProvidedPassword, function(userProvidedPasswordHash){
       var task = {};
       task.password = userProvidedPasswordHash;
-      db.writeAuthTask(username, authName, task, function(error, authTask, user){
-        next();
+      db.saveAuthTask(username, 'password', task, function(error, authTask, user){
+        // What should happen here?
       });
     });
   },
@@ -68,7 +67,7 @@ utils.comparePassword = function(userProvidedPasswordHash, storedPasswordHash, c
   });
 };
 utils.hashPassword = function(userProvidedPassword, callback){
-  bcrypt.hash(userProvidedPassword, null, function(err, hash){
+  bcrypt.hash(userProvidedPassword, null, null, function(err, hash){
     callback(hash);
   });
 }
