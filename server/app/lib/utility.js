92b2fc4d49247b69d886f4cb1416a16423623989
Ryo Osawa <ryojiosawa@gmail.com>
2014-10-23 09:40:27 -0700

    Add generateAuthToken util function

--- server/app/lib/utility.js
+++ server/app/lib/utility.js
@@ -2,16 +2,20 @@ var bcrypt = require('bcrypt-nodejs');
 
 var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
 
-exports.isValidUrl = function(url) {
+var isValidUrl = function(url) {
   return url.match(rValidUrl);
 };
 
-exports.isLoggedIn = function(req) {
+var isLoggedIn = function(req) {
   return req.session ? !!req.session.user : false;
 };
 
+/**
+ * This util function is used to determine if a request contains a session.  If not, will redirect to login page
+ *
+ */
 exports.checkUser = function(req, res, next) {
-  if (!exports.isLoggedIn(req)) {
+  if (isLoggedIn(req)) {
     res.redirect('/login');
   } else {
     next();
@@ -30,8 +34,22 @@ exports.comparePassword = function(userProvidedPasswordHash, storedPasswordHash,
     callback(isMatch);
   });
 };
+
 exports.hashPassword = function(userProvidedPassword, callback){
   bcrypt.hash(userProvidedPassword, null, null, function(err, hash){
     callback(hash);
   });
-}
+};
+
+/**
+ * This util function generates an authentication token
+ *
+ * @param callback - callback function to be invoked once auth token is generated
+ */
+exports.generateAuthToken = function(callback) {
+  var token = 'fakeToken';
+
+  if (callback) {
+    callback(token);
+  }
+};
