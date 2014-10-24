c7b42f0e4aac4eb401b80601c961bd69f1497c23
suprbh <supb27@yahoo.com>
2014-10-24 14:26:52 -0700

    face recognition module related changes

--- server/server-config.js
+++ server/server-config.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var partials = require('express-partials');
-var morgan = require('morgan');
 var handler = require('./app/controllers/controller');
 var util = require('./app/lib/utility');
 var passwordModule = require('./modules/password/auth.js');
+var faceModule = require('./modules/faceRecognition/auth.js');
 
 var app = express();
 
@@ -14,7 +14,6 @@ app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(partials());
-  app.use(morgan('dev'));
   app.use(express.bodyParser());
   app.use(express.static(__dirname + '/public'));
   app.use(express.cookieParser('shhhh, very secret'));
@@ -23,6 +22,7 @@ app.configure(function() {
 /*
   Define all routes here
  */
+app.get('/module/:module', util.checkUser, handler.dispatchModule);
 
 app.get('/modules/:module', util.checkUser, handler.dispatchModule);
 
@@ -38,6 +38,12 @@ app.get('/modules/tempo/setup', /*util.checkUser,*/ tempoModule.setupRender);
 app.get('/modules/tempo/auth', /*util.checkUser,*/ tempoModule.authRender);
 app.post('/modules/tempo/setup', /*util.checkUser,*/ tempoModule.setup);
 app.post('/modules/tempo/auth', /*util.checkUser,*/ tempoModule.auth);
+
+/* Face recognition module routes */
+app.get('/modules/face/setup', util.checkUser, faceModule.setupRender);
+app.get('/modules/face/auth', util.checkUser, faceModule.authRender);
+app.post('/modules/face/setup', util.checkUser, faceModule.setup);
+app.post('/modules/face/auth', util.checkUser, faceModule.auth);
 
 app.get('/login', handler.loginForm);
 app.post('/login', handler.login);
