c95136c35e4561b20311aa9574edbee9b2ec9d35
Allen <allenkrulwich@gmail.com>
2014-10-26 16:31:17 -0700

    funny message edit

--- server/server-config.js
+++ server/server-config.js
@@ -28,7 +28,7 @@ app.get('/modules/:module', util.checkUser, handler.dispatchModule);
 
 /* Password module routes */
 app.get('/modules/face/setup', util.checkUser, function(req, res){
-  res.send('<html><body><h1> It could be you who implements this feature!</h1></body></html>');
+  res.send('<html><body><h1> It could be you who finishes implementing this feature!</h1></body></html>');
 });
 
 /* Password module routes */
